f86f20afb21aa02b2a41561a006a7fa33e8941bc
WONGCH9 <wongch9@oocl.com>
2022-12-05 16:09:43 +0800

    refactor: extract constant to remove magic number

--- src/main/java/com/afs/refactoring/Police.java
+++ src/main/java/com/afs/refactoring/Police.java
@@ -2,7 +2,9 @@ package com.afs.refactoring;
 
 public class Police {
 
+    public static final int MIN_ADULT_AGE = 18;
+
     public boolean checkDriver(Driver driver) {
-        return (driver.age >= 18);
+        return (driver.age >= MIN_ADULT_AGE);
     }
 }
